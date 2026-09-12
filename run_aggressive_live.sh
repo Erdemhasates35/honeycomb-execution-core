14089daf393af080dca7d40170f4ae4ecfad6cc9
Erdemhasates35 <erdemhasates343@gmail.com>
2026-09-12 15:51:07 +0300

    feat: apply profit-first 40x leverage policy to aggressive engine

--- run_aggressive_live.sh
+++ run_aggressive_live.sh
@@ -4,4 +4,8 @@ cd /data/data/com.termux/files/home/honeycomb-execution-core
 export EXECUTION_MODE=LIVE
 export HONEYCOMB_MODE=LIVE
 export LIVE_ARMED=1
+export MIN_LEVERAGE="${MIN_LEVERAGE:-40}"
+export LEV_MIN="${LEV_MIN:-40}"
+export MAX_LEVERAGE="${MAX_LEVERAGE:-75}"
+export EXECUTION_STYLE="${EXECUTION_STYLE:-AUTO}"
 exec python3 aggressive_live_engine.py
